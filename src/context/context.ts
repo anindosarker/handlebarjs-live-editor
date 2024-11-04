272f437ef77342bada89d7393d89c821c09f99f8
anindo-mac <sarkeranindo@gmail.com>
2024-11-05 02:43:55 +0600

    Update package.json and context.ts for project renaming and versioning

--- src/context/context.ts
+++ src/context/context.ts
@@ -1,4 +1,5 @@
 export const context = {
   title: "Vite + Handlebars Playground",
-  description: "Just modify the template.hbs file and see the changes live!🔥",
+  description:
+    "🔥 Just modify the template.hbs file and see the changes live! 🔥",
 };
